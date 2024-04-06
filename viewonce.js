d0dad81da1bce34acf9c4aa9b8c72ecd75193387
Chronisch1337 <75181938+chronischer@users.noreply.github.com>
2024-04-06 20:14:51 -0300

    Update viewonce.js

--- viewonce.js
+++ viewonce.js
@@ -29,9 +29,9 @@ await slowed.sendjson(botNumber, obj)
 break
 case "viewonce":
 if (!isOwner) return enviar('só o meu dono pode usar isso');
-config = text == "false" ? false : text == "true" ? true : "erro"
-if(config == "erro") return enviar(`true | ativa\nfalse | desativa`)
-logph.viewonce = config
+configsh = text == "false" ? false : text == "true" ? true : "erro"
+if(configsh == "erro") return enviar(`true | ativa\nfalse | desativa`)
+logph.viewonce = configsh
 await fs.writeFileSync('./plugins/config.json', JSON.stringify(logph, null, 2))
 await enviar(```Config setada para ${text}```)
 break
